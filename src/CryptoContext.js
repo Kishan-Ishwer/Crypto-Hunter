83a30890175e81fd4f588fd6e6c5ecdd39a6eb1d
Kishan-Ishwer <38846069+Kishan-Ishwer@users.noreply.github.com>
2022-02-21 18:12:25 +1300

    Add files via upload

---
+++ src/CryptoContext.js
@@ -0,0 +1,25 @@
+import React, { createContext, useContext, useEffect, useState } from "react";
+
+const Crypto = createContext();
+
+const CryptoContext = ({ children }) => {
+  const [currency, setCurrency] = useState("NZD");
+  const [symbol, setSymbol] = useState("$");
+
+  useEffect(() => {
+    if (currency === "NZD") setSymbol("$");
+    else if (currency === "USD") setSymbol("$");
+  }, [currency]);
+
+  return (
+    <Crypto.Provider value={{ currency, symbol, setCurrency }}>
+      {children}
+    </Crypto.Provider>
+  );
+};
+
+export default CryptoContext;
+
+export const CryptoState = () => {
+  return useContext(Crypto);
+};
